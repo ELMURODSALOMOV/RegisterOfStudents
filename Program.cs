bfd5f72b7d7e11bbddb533f782a2cf72e96a30f1
ELMURODSALOMOV <salomovelmurod062@gmail.com>
2024-04-22 00:45:03 +0500

    Student Information

--- Program.cs
+++ Program.cs
@@ -1,2 +1,57 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using RegisterOfStudents.Models;
+using System.Collections.Generic;
+
+List<Student> students = new List<Student>()
+{
+    new Student()
+    {
+        Id = 1,
+        FirstName = "Elmurod",
+        LastName = "Salomov",
+        Age = 27,
+        Email = "Salomov@gmail.com"
+    },
+    new Student()
+    {
+        Id = 2,
+        FirstName = "Baxtiyor",
+        LastName = "Ruziyev",
+        Age = 27,
+        Email = "Ruziyev@gmail.com"
+    },
+    new Student()
+    {
+        Id = 3,
+        FirstName = "Asilbek",
+        LastName = "Abduvohidov",
+        Age = 27,
+        Email = "Abduvohidov@gmail.com"
+    },
+    new Student()
+    {
+        Id = 4,
+        FirstName = "Donyor",
+        LastName = "Homidov",
+        Age = 27,
+        Email = "Homidov@gmail.com"
+    },
+    new Student()
+    {
+        Id = 5,
+        FirstName = "Shoxrux",
+        LastName = "Abduvaliyev",
+        Age = 27,
+        Email = "Abduvaliyev@gmail.com"
+    }
+};
+
+students.ForEach(student => Console.WriteLine($"{student.FirstName}\n{student.Email}"));
+
+foreach(Student student in students)
+{
+    if(student.FirstName.ToLower().Contains("a"))
+    {
+        Console.WriteLine(student.FirstName);
+    }
+}
