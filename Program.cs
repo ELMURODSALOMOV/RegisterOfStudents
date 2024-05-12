c498e1e99dfbfe7851ca29fc85ee2f502bdebdbe
ELMURODSALOMOV <salomovelmurod062@gmail.com>
2024-05-12 18:59:43 +0500

    Program Impliment

--- Program.cs
+++ Program.cs
@@ -1,11 +1,51 @@
-﻿
-using RegisterOfStudents.Broker.Storeage;
-using RegisterOfStudents.Models;
+﻿using RegisterOfStudents.Models;
 using RegisterOfStudents.Service;
-using System.Collections.Generic;
-using System.Runtime.CompilerServices;
 
 StudentService studentService = new StudentService();
-Console.WriteLine("Ismni kiriting:");
-string studentName = Console.ReadLine();
-studentService.CheckoutByName(studentName);
+bool isContinue = true;
+do
+{
+    Console.WriteLine("1. Display Student");
+    Console.WriteLine("2. Checkout by letter");
+    Console.WriteLine("3. Checkout by name");
+    Console.WriteLine("4. Insert Student");
+    Console.Write("Enter command: ");
+    string command = Console.ReadLine();
+    if (command.Contains("1") is true)
+    {
+        Console.Write("Enter the id: ");
+        int id = Convert.ToInt32(Console.ReadLine());
+        studentService.DisplayStudent(id);
+    }
+    if (command.Contains("2") is true)
+    {
+        Console.Write("Enter the charector: ");
+        char letter = Convert.ToChar(Console.ReadLine());
+        studentService.CheckoutByLetter(letter);
+    }
+    if (command.Contains("3") is true)
+    {
+        Console.Write("Enter the first name: ");
+        string studentName = Console.ReadLine();
+        studentService.CheckoutByName(studentName);
+    }
+    if (command.Contains("4") is true)
+    {
+        Student student = new Student();
+        Console.Write("Enter the first name: ");
+        student.FirstName = Console.ReadLine();
+        Console.Write("Enter the last name: ");
+        student.LastName = Console.ReadLine();
+        Console.Write("Enter the age: ");
+        student.Age = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Enter the email: ");
+        student.Email = Console.ReadLine();
+        studentService.InsertStudent(student);
+    }
+    Console.Write("Is Continue: ");
+    string isCommand = Console.ReadLine();
+    if (isCommand.Contains("no") is true)
+    {
+        isContinue = false;
+    }
+}while(isContinue is true);
